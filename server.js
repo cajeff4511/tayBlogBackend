81ca6ab1cfae471150bb754e697c23562874c31d
Christian Jefferson <jeffersonchristian259@gmail.com>
2025-02-19 14:04:36 -0600

    mongo

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const cors = require('cors')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 const app = express()
 const port = '3000'
 app.use(cors())
+app.use(express.json())
+
+
+mongoose.connect('mongodb+srv://jeffersonchristian259:Ivh5vgdJAnd9Px2G@taysblog.ldkit.mongodb.net/Blog')
+.then(() => {
+    console.log('db connected!')
+})
+.catch(() => {
+    console.log('db connection error!')
+})
 app.get('/', (req, res) => {
     res.send("hello")
 })
